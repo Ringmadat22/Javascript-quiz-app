b724d41b2b906b9bce355cb5fe5233ecf71b7041
Ring Madat <smithemma329@gmail.com>
2023-10-23 05:40:37 +0300

    main

--- Desktop/quiz app/app.js
+++ Desktop/quiz app/app.js
@@ -86,8 +86,12 @@ function selectAnswer(e){
         selectedBtn.classList.add("incorrect");
     }
     Array.from(answerButtons.children).forEach(button => {
-        if(button.dataset.correct === )
-    })
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
 }
 
 startQuiz();
