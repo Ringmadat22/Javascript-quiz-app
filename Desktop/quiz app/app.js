ce1631bb365a3b447574755f07407548bb582908
Ring Madat <smithemma329@gmail.com>
2023-10-23 05:29:25 +0300

    main

--- Desktop/quiz app/app.js
+++ Desktop/quiz app/app.js
@@ -7,8 +7,7 @@ const questions = [
             {text: "Australia", correct:"false"},
             {text: "Antactica", correct:"false"}
         ]
-    }
-    ,
+    },
     {
         question: "which is your name",
         answers: [
@@ -39,7 +38,7 @@ const questions = [
 ];
 
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
@@ -53,14 +52,29 @@ function startQuiz(){
 }
 
 function showQuestion(){
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
     currentQuestion.answers.forEach(answer => {
-        const button = document.createComment("button");
+        const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerButton.appendChild(button);
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
     });
 }
+
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+startQuiz();
