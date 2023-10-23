7f2a3cb4365b513340b7f532357cb39e5b041bdb
Ring Madat <smithemma329@gmail.com>
2023-10-23 05:12:23 +0300

    main

---
+++ Desktop/quiz app/app.js
@@ -0,0 +1,39 @@
+const questions = [
+    {
+        question: "which is the largest continent in the world?",
+        answers: [
+            {text: "Asia", correct:"false"},
+            {text: "Africa", correct:"true"},
+            {text: "Australia", correct:"false"},
+            {text: "Antactica", correct:"false"}
+        ]
+    }
+    ,
+    {
+        question: "which is your name",
+        answers: [
+            {text: "Ring", correct:"false"},
+            {text: "Ghost", correct:"false"},
+            {text: "Madat", correct:"false"},
+            {text: "am Him", correct:"true"}
+        ]
+    },
+    {
+        question: "which are you from",
+        answers: [
+            {text: "South Sudan", correct:"true"},
+            {text: "Uganda", correct:"false"},
+            {text: "Canada", correct:"false"},
+            {text: "Nairobu", correct:"false"}
+        ]
+    },
+    {
+        question: "which does Ringo do for WORK",
+        answers: [
+            {text: "Politician", correct:"false"},
+            {text: "Corrupt official", correct:"false"},
+            {text: "Unemployed", correct:"false"},
+            {text: "Software engineer", correct:"true"}
+        ]
+    },
+]
